059d6ccc51d12e138c8384ba128c6a7695609eb8
SvenBouwen <sven.bouwen@vdabcampus.be>
2023-02-25 14:35:19 +0100

    hfdstk18

--- src/test/java/be/vdab/profiles/repositories/DataSourceTest.java
+++ src/test/java/be/vdab/profiles/repositories/DataSourceTest.java
@@ -1,2 +1,21 @@
-package be.vdab.profiles.repositories;public class DataSourceTest {
+package be.vdab.profiles.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@DataJpaTest
+public class DataSourceTest {
+    private final DataSource dataSource;
+    public DataSourceTest(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Test
+    void databaseNaam() throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            System.out.println(connection.getCatalog());
+        }
+    }
 }
